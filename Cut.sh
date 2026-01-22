2d977748754a16c7e018da8fbf164379c7bdbd55
Shruti Vedha <shrutivedha1@gmail.com>
2026-01-22 12:47:55 +0530

    cut command to print 3rd character from each line

---
+++ Cut.sh
@@ -0,0 +1,3 @@
+Q } Given N lines of input, print the 3rd character from each line as a new line of output. It is guaranteed that each of the n lines of input will have a  character.
+
+A } cut -c 3 
