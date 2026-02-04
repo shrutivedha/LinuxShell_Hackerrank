4f22f416a9f722888e863a32d831daa11e3ef76f
Shruti Vedha <shrutivedha1@gmail.com>
2026-02-04 20:36:42 +0530

    Create tr_2.sh to delete lowercase characters
    Added a script to demonstrate the usage of the tr command.

---
+++ tr_2.sh
@@ -0,0 +1,4 @@
+Q} In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+In a given fragment of text, delete all the lowercase characters a-z.
+
+A} tr -d 'a-z'
