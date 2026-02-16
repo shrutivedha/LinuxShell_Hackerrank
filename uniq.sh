ba980a7752fec19493faf13eb3503290bf79e0ab
Shruti Vedha <shrutivedha1@gmail.com>
2026-02-16 22:10:57 +0530

    Add uniq command usage details to uniq.sh
    Add usage instructions for the uniq command.

---
+++ uniq.sh
@@ -0,0 +1,3 @@
+Q} In this challenge, we practice using the uniq command to eliminate consecutive repetitions of a line when a text file is piped through it.
+#This command Removes all duplicates from a file
+A} uniq
