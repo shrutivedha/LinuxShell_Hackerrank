79bb5b7bb91c5a87a26ae1562b44407aa864f6cc
Shruti Vedha <shrutivedha1@gmail.com>
2026-03-03 23:01:40 +0530

    Create Paste4.sh to fold CSV rows
    Add script to restructure CSV by folding rows

---
+++ Paste4.sh
@@ -0,0 +1,5 @@
+Q} Given a CSV file where each row contains the name of a city and its state separated by a comma,
+your task is to restructure the file in such a way, 
+that three consecutive rows are folded into one, and separated by tab.
+
+A}  paste -s -d '\t\t\n' - - -
